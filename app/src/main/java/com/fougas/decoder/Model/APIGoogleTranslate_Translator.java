dd133d64879bda337bdb6f92eccbea2611044124
j.still <jean.still@etu.unistra.fr>
2017-04-22 17:03:18 +0200

    Use API WhatsMate  On branch translation_inprocess

---
+++ app/src/main/java/com/fougas/decoder/Model/APIGoogleTranslate_Translator.java
@@ -0,0 +1,8 @@
+package com.fougas.decoder.Model;
+
+/**
+ * Created by Jean on 22/04/2017.
+ */
+
+public class APIGoogleTranslate_Translator {
+}
---
+++ app/src/main/java/com/fougas/decoder/Model/APIWhatsMate_TranslatorConnected.java
@@ -0,0 +1,66 @@
+package com.fougas.decoder.Model;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Jean on 22/04/2017.
+ * This class translate a file.txt
+ */
+
+
+
+public class TranslatorConnected {
+    /**
+     * Sends out a WhatsApp message via WhatsMate WA Gateway.
+     */
+    public static void translate(String fromLang, String toLang, String text) throws Exception {
+
+        // TODO: If you have your own Premium account credentials, put them down here:
+        final String CLIENT_ID = "FREE_TRIAL_ACCOUNT";
+        final String CLIENT_SECRET = "PUBLIC_SECRET";
+        final String ENDPOINT = "http://api.whatsmate.net/v1/translation/translate";
+
+
+        // TODO: Should have used a 3rd party library to make a JSON string from an object
+        String jsonPayload = new StringBuilder()
+                .append("{")
+                .append("\"fromLang\":\"")
+                .append(fromLang)
+                .append("\",")
+                .append("\"toLang\":\"")
+                .append(toLang)
+                .append("\",")
+                .append("\"text\":\"")
+                .append(text)
+                .append("\"")
+                .append("}")
+                .toString();
+
+        URL url = new URL(ENDPOINT);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("X-WM-CLIENT-ID", CLIENT_ID);
+        conn.setRequestProperty("X-WM-CLIENT-SECRET", CLIENT_SECRET);
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        OutputStream os = conn.getOutputStream();
+        os.write(jsonPayload.getBytes());
+        os.flush();
+        os.close();
+
+        int statusCode = conn.getResponseCode();
+        System.out.println("Status Code: " + statusCode);
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                (statusCode == 200) ? conn.getInputStream() : conn.getErrorStream()
+        ));
+        String output;
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+        conn.disconnect();
+    }
+}
