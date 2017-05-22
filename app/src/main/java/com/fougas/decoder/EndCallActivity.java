7fdc95a5140bd3621ec6ee5d3d472abfddfa7e19
Quentin <arcahyxe@gmail.com>
2017-05-22 16:57:02 +0200

    Adding button, changing icon

--- app/src/main/java/com/fougas/decoder/EndCallActivity.java
+++ app/src/main/java/com/fougas/decoder/EndCallActivity.java
@@ -1,7 +1,10 @@
 package com.fougas.decoder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class EndCallActivity extends Activity {
@@ -11,9 +14,24 @@ public class EndCallActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_call);
 
+        Button aEndBtnCancel = (Button) findViewById(R.id.aEndBtnCancel);
+        Button aEndBtnValidate = (Button) findViewById(R.id.aEndBtnValidate);
         TextView aEndTvDuration = (TextView) findViewById(R.id.aEndTvDuration);
 
-
+        aEndBtnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickBtnCancel();
+            }
+        });
         aEndTvDuration.setText("39:25");
+    }
+
+    /**
+     * On click on the button Cancel
+     */
+    private void onClickBtnCancel(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
