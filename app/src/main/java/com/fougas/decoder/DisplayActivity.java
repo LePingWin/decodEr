8dce7c2749492f8f99ee30cded3e36a06b5d979a
Quentin <dell.quentin@gmail.com>
2017-09-29 14:44:00 +0200

    Adding function for displaying text on the display view

--- app/src/main/java/com/fougas/decoder/DisplayActivity.java
+++ app/src/main/java/com/fougas/decoder/DisplayActivity.java
@@ -2,10 +2,10 @@ package com.fougas.decoder;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DisplayActivity extends Activity {
 
@@ -15,6 +15,7 @@ public class DisplayActivity extends Activity {
         setContentView(R.layout.activity_display);
 
         Button aDispBtnClose = (Button) findViewById(R.id.aDispBtnClose);
+        Button aDispBtnListen = (Button) findViewById(R.id.aDispBtnListen);
         Button aDispBtnParameters = (Button) findViewById(R.id.aDispBtnParameters);
 
         aDispBtnClose.setOnClickListener(new View.OnClickListener() {
@@ -23,11 +24,16 @@ public class DisplayActivity extends Activity {
                 onClickBtnClose();
             }
         });
-
         aDispBtnParameters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onClickBtnParameters();
+            }
+        });
+        aDispBtnListen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickBtnListen();
             }
         });
     }
@@ -46,5 +52,21 @@ public class DisplayActivity extends Activity {
     private void onClickBtnParameters() {
         Intent intent = new Intent(this, ParameterActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * On click on the button listen
+     */
+    private void onClickBtnListen() {
+        // TODO Call listen from google
+    }
+
+    /**
+     * Allow to fill the textview with a string
+     * @param text The string that will be displayed
+     */
+    private void fillTextView(String text){
+        TextView textView = (TextView) findViewById(R.id.aDispTvText);
+        textView.setText(text);
     }
 }
