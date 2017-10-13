734155b4d45ef9580b13b42f6b0064814b542b8e
jstill <jean.still68@yahoo.fr>
2017-10-13 17:02:02 +0200

    Merge branch 'develop' of https://github.com/LePingWin/decodEr into develop
    # Conflicts:
    #	app/src/main/java/com/fougas/decoder/Activity/ExampleTranslationActivity.java
    #	app/src/main/java/com/fougas/decoder/Service/TranslateService.java

--- app/src/main/java/com/fougas/decoder/Activity/DisplayActivity.java
+++ app/src/main/java/com/fougas/decoder/Activity/DisplayActivity.java
@@ -2,8 +2,10 @@ package com.fougas.decoder.Activity;
 
 import android.Manifest;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,8 +20,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.fougas.decoder.Model.Langage;
 import com.fougas.decoder.R;
+import com.fougas.decoder.Service.Interface.IOnTaskCompleted;
 import com.fougas.decoder.Service.SpeechService;
+import com.fougas.decoder.Service.TranslateService;
 import com.fougas.decoder.Service.VoiceRecorder;
 
 import java.io.BufferedReader;
@@ -27,16 +33,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class DisplayActivity extends FragmentActivity implements MessageDialogFragment.Listener {
+public class DisplayActivity extends FragmentActivity implements MessageDialogFragment.Listener, IOnTaskCompleted  {
     private static final String FRAGMENT_MESSAGE_DIALOG = "message_dialog";
 
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
     private final int FILE_SELECT_CODE = 1;
 
+    private TextView mTranslateText;
+    private SharedPreferences msharedPreferences ;
+    IOnTaskCompleted listener;
+
     private SpeechService mSpeechService;
 
     private VoiceRecorder mVoiceRecorder;
+
+
+
     private final VoiceRecorder.Callback mVoiceCallback = new VoiceRecorder.Callback() {
+
+
+
 
         @Override
         public void onVoiceStart() {
@@ -85,6 +101,14 @@ public class DisplayActivity extends FragmentActivity implements MessageDialogFr
         Button aDispBtnClose = (Button) findViewById(R.id.aDispBtnClose);
         Button aDispBtnListen = (Button) findViewById(R.id.aDispBtnListen);
         Button aDispBtnParameters = (Button) findViewById(R.id.aDispBtnParameters);
+
+        mTranslateText = (TextView) findViewById(R.id.aDispTvText);
+
+        listener = this; // Listener for translation
+        msharedPreferences = getSharedPreferences(getString(R.string.appName), Context.MODE_PRIVATE);
+
+
+
 
         aDispBtnClose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -295,7 +319,16 @@ public class DisplayActivity extends FragmentActivity implements MessageDialogFr
                                 if (isFinal) {
                                     mText.setText(null);
                                 } else {
-                                    mText.setText(text);
+                                    try {
+                                        TranslateService taskGoogleAPITranslate =
+                                                new TranslateService(
+                                                        listener
+                                                        , text
+                                                        , Langage.getELanguage((msharedPreferences.getString(getString(R.string.sharedPreferencesTranslationLanguage), ""))));
+                                        taskGoogleAPITranslate.execute();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
                                     Log.d("TRAD",text);
 
                                 }
@@ -306,4 +339,17 @@ public class DisplayActivity extends FragmentActivity implements MessageDialogFr
                 }
             };
 
+    @Override
+    public void onTaskCompleted(Object value) {
+        mTranslateText.setText(mTranslateText.getText() + value.toString());
+    }
+
+    /**
+     * Value return when task is completed
+     * @param value Value return when task is completed
+     */
+   /* @Override
+    public void onTaskCompleted(Object value) {
+        mTranslateText.setText(value.toString());
+    }*/
 }
